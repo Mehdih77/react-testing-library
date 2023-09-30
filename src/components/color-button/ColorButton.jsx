6f55fcc537c640abb452fd019000a48816f9b18d
Mehdi <mehdi.havaei77@gmail.com>
2023-09-30 11:32:44 +0330

    test checkbox

--- src/components/color-button/ColorButton.jsx
+++ src/components/color-button/ColorButton.jsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
 
 export default function ColorButton() {
+  const [disabled, setDisabled] = useState(false);
   const [buttonColor, setButtonColor] = useState("red");
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
   return (
     <div>
       <button
+        disabled={disabled}
         onClick={() => setButtonColor(newButtonColor)}
         style={{ backgroundColor: buttonColor }}>
         Change to {newButtonColor}
       </button>
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={() => setDisabled(!disabled)}
+        // onChange={(e) => setDisabled(e.target.checked)}
+      />
     </div>
   );
 }
--- src/components/color-button/ColorButton.test.js
+++ src/components/color-button/ColorButton.test.js
@@ -1,7 +1,7 @@
 import { screen, render, fireEvent } from "@testing-library/react";
 import ColorButton from "./ColorButton";
 
-test("render color button", () => {
+test("color button has correct initial color", () => {
   render(<ColorButton />);
   const buttonElm = screen.getByRole("button", { name: "Change to blue" });
   expect(buttonElm).toHaveStyle({ backgroundColor: "red" });
@@ -9,4 +9,21 @@ test("render color button", () => {
   fireEvent.click(buttonElm);
   expect(buttonElm).toHaveStyle({ backgroundColor: "blue" });
   expect(buttonElm.textContent).toBe("Change to red");
+});
+test("color button checkbox inintial condition", () => {
+  render(<ColorButton />);
+  const buttonElm = screen.getByRole("button", { name: "Change to blue" });
+  expect(buttonElm).toBeEnabled();
+  const checkboxElm = screen.getByRole("checkbox");
+  expect(checkboxElm).not.toBeChecked();
+});
+test("checkbox disabled button on first click and enabled in second click", () => {
+  render(<ColorButton />);
+  const buttonElm = screen.getByRole("button", { name: "Change to blue" });
+  const checkboxElm = screen.getByRole("checkbox");
+  // changed the checkbox
+  fireEvent.click(checkboxElm);
+  expect(buttonElm).toBeDisabled();
+  fireEvent.click(checkboxElm);
+  expect(buttonElm).toBeEnabled();
 });
