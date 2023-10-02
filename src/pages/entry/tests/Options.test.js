4ccccbe33758516d7508b1bee09fbe14d56482d5
Mehdi <mehdi.havaei77@gmail.com>
2023-10-02 07:51:25 +0330

    test for dont update total when input is invalid

--- src/pages/entry/tests/Options.test.js
+++ src/pages/entry/tests/Options.test.js
@@ -1,4 +1,5 @@
 import { screen, render } from "../../../test-utils/testing-library-utils";
+import userEvent from "@testing-library/user-event";
 import Options from "../Options";
 
 test("displays image for each scoop option from server", async () => {
@@ -29,4 +30,18 @@ test("displays image for each topping option from server", async () => {
     "M&Ms topping",
     "Hot fudge topping",
   ]);
+});
+
+test("don't update total if scoops input is invalid", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="scoops" />);
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "-1");
+
+  const scoopsSubtotal = screen.getByText("Scoops total: $0.00");
+  expect(scoopsSubtotal).toBeInTheDocument();
 });
