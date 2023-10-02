880d3c195798112a267c4b2d40c10a4897370c3b
Mehdi <mehdi.havaei77@gmail.com>
2023-10-02 07:27:51 +0330

    test for disabled button for order

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -4,13 +4,16 @@ import Options from "./Options";
 
 export default function OrderEntry({ setOrderPhase }) {
   const [orderDetails] = useOrderDetails();
+  const diabledOrder = orderDetails.totals.scoops === "$0.00";
   return (
     <div>
       <h1>Design Your Sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
-      <Button onClick={() => setOrderPhase("review")}>Order Sundae!</Button>
+      <Button disabled={diabledOrder} onClick={() => setOrderPhase("review")}>
+        Order Sundae!
+      </Button>
     </div>
   );
 }
--- src/pages/entry/tests/OrderEntry.test.js
+++ src/pages/entry/tests/OrderEntry.test.js
@@ -6,6 +6,7 @@ import {
 import { server } from "../../../mocks/server";
 import { rest } from "msw";
 import OrderEntry from "../OrderEntry";
+import userEvent from "@testing-library/user-event";
 test("handle errors for scoops and topping routes", async () => {
   server.resetHandlers(
     rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
@@ -24,9 +25,29 @@ test("handle errors for scoops and topping routes", async () => {
     expect(alerts).toHaveLength(2);
   });
 });
+test("should disabled button if we dont have any scoops", async () => {
+  const user = userEvent.setup();
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+  const orderButton = screen.getByRole("button", { name: /Order Sundae/i });
+  expect(orderButton).toBeDisabled();
+
+  // add scoop
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: /vanilla/i,
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  expect(orderButton).toBeEnabled();
+
+  // remove scoop and expect button to be disabled
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "0");
+  expect(orderButton).toBeDisabled();
+});
 
 //! jest.fn():
-// using that jest mock function as a placeholder when there is 
+// using that jest mock function as a placeholder when there is
 // a function props and as you will recall this is needed when
 // the component that you are rendering has a props
 // that is a function that is going to get run during the course of the test
